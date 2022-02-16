489c1631c90f3f3eb309db4c2934c0797929b33f
vance.petitt <vance.petitt@gmail.com>
2022-02-16 09:53:44 -0600

    fixed error in inventory_quantity field, created products app and registered as app in proect settings. Made migrations

---
+++ products/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.forms import IntegerField
+
+# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length = 255)
+    description = models.CharField(max_length = 255)
+    price = models.DecimalField(max_digits = 8, decimal_places = 2)
+    inventory_quantity = models.IntegerField()
