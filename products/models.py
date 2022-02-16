9c60ead4afb7ea90c0abdabaeb8d1243cf72860d
vance.petitt <vance.petitt@gmail.com>
2022-02-16 16:20:54 -0600

    Updated Products model and serializer to accept an image link from the internet. Updated Postman file.

--- products/models.py
+++ products/models.py
@@ -8,3 +8,4 @@ class Product(models.Model):
     description = models.CharField(max_length = 255)
     price = models.DecimalField(max_digits = 8, decimal_places = 2)
     inventory_quantity = models.IntegerField()
+    image_link = models.CharField(max_length = 999)
